61429b7923ff1c36c3312a7c0b276029c73b8f4f
chandra sekhar <chandrasekharjayapraveen@gmail.com>
2024-11-02 16:43:47 +0530

    committing example1 to dev1

--- example1.java
+++ example1.java
@@ -2,6 +2,6 @@ public class B
 {
     public static void main(String[] args)
     {
-        System.out.println("never feel fear remember who is your backside");
+        System.out.println("never feel fear remember who is your backside ok");
     }
 }
