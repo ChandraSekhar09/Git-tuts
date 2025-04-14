2d8ab08c284e0a9d749a3ee297ff26e06424d7cd
ChandraSekhar09 <chandrasekharjayapraveen@gmail.com>
2025-04-13 23:04:55 -0700

    coding example1 and example3

--- coding.java
+++ coding.java
@@ -1,4 +1,4 @@
-public class A
+public class coding
 {
     public static void main(String[] args)
     {
---
+++ example3.java
@@ -0,0 +1,8 @@
+public class example3
+{ 
+    public static void main(String[] args)
+    {
+        System.out.println("example3 in copy3");
+    }
+    
+}
