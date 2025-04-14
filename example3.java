44d38cc610166314f7f6eca63adc1dec726e5619
ChandraSekhar09 <chandrasekharjayapraveen@gmail.com>
2025-04-14 04:45:25 -0700

    committing example3

--- example3.java
+++ example3.java
@@ -2,7 +2,7 @@ public class example3
 { 
     public static void main(String[] args)
     {
-        System.out.println("example3 in copy3 in master");
+        System.out.println("example3 in master");
     }
     
 }
